ffacc64f66aa8207ddf2497a73910de2442c2562
Zachary Friss <friss@frissdesigns.com>
2014-02-01 12:57:16 -0500

    Initial Commit

---
+++ randomGif.py
@@ -0,0 +1,75 @@
+import os
+import random
+import subprocess
+from moviepy.editor import *
+
+
+def scanfolder(root):
+	"""
+	Get all movies with certain file extenstion andd append to list
+	Params: root - path to root movies Directory
+	Returns: movies - list of movies found.
+	"""
+	movies = []
+	for path, dirs, files in os.walk(root):
+		for f in files:
+			if f.endswith('.mkv') or f.endswith('.m2ts') or f.endswith('.avi'):
+			    #print os.path.join(path, f)
+			    movies.append(os.path.join(path,f))
+	#print movies	        
+	return movies
+
+def getLength(filename):
+	"""
+	Get information on choosen movie file. 
+	Params: filename - path to movie to check
+	Returns: list containing the line "Duration"
+	Note: For some reason json output wouldn't show Duration while this call does.
+	"""
+	result = subprocess.Popen(["ffprobe", filename],stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+	return [x for x in result.stdout.readlines() if "Duration" in x]
+
+
+def makeGif(root):
+	files = scanfolder(root) #Get all Movies
+
+	moviepath = random.choice(files) #Random Movie from list
+
+	moviename = moviepath[root.__len__():].split("/")[2] #Remove Root Directory and Subfolder
+	moviename = moviename[:moviename.index(".")] #Remove file extenstion 
+	moviename = moviename.split("[")[0] #Remove any [2013/1080p/720p]
+	moviename = moviename.split("(")[0] #Remove any (2013/1080p/720p)
+	print "Movie Chosen: " + moviename
+
+	duration = getLength(moviepath)[0].split(",") #Get Duration
+	duration = duration[0].split(" ")
+	duration = duration[3].split(":") #Break into hour, mins, secs
+	#print duration
+	
+	hour = random.randint(0,int(duration[0])) #Random hour
+	mins = random.randint(0,int(duration[1])+1) #Random min
+	secs = float(duration[2]) #Parse secs to float
+
+	timePassed = round(random.uniform(0, 5),2) #Random seconds to elapse up to 5.
+	
+	"""
+	print hour
+	print mins
+	print secs
+	print timePassed
+	"""
+	
+	#Make GIF 1/3 sized.
+	VideoFileClip(moviepath).\
+				subclip((hour,mins,secs),(hour,mins,secs+timePassed)).\
+				resize(0.3).\
+				to_gif('movie.gif')
+	
+	return moviename, hour, mins, secs
+	
+
+if __name__ == "__main__":
+	var = raw_input("Enter Path to Movie Directory: ")
+	print "Movies Path: ", var
+	moviename, hour, mins, secs = makeGif(var)
+	exit()
---
+++ twitterBot.py
@@ -0,0 +1,106 @@
+import base64
+import json
+import requests
+import ConfigParser
+import random
+import os
+import time
+import subprocess
+
+from twython import Twython
+from base64 import b64encode
+from randomGif import makeGif
+
+
+config = ConfigParser.ConfigParser()
+config.read("config.cfg")
+config.sections()
+ROOT_FOLDER = config.get("general", "movies_path")
+CLIENT_ID = config.get("imgur", "client_id")
+API_KEY = config.get("imgur", "api_key")
+APP_KEY = config.get("twitter", "app_key")
+APP_SECRET = config.get("twitter", "app_secret")
+OAUTH_TOKEN = config.get("twitter", "oauth_token")
+OAUTH_TOKEN_SECRET = config.get("twitter", "oauth_token_secret")
+
+headers = {"Authorization": "Client-ID " + CLIENT_ID}
+url = "https://api.imgur.com/3/upload.json"
+
+while True:
+	moviename, hour, mins, secs = makeGif(ROOT_FOLDER)
+
+	title = "Random " + moviename + "GIF"
+	print title
+
+	# first pass reduce the amount of colors
+	if(os.path.getsize('movie.gif') > 2097152):
+		subprocess.call(['convert',
+						'movie.gif',
+						'-layers',
+						'Optimize',
+						'-colors',
+						'64',
+						'movie.gif',
+						'>',
+						'/dev/null',
+						'2>&1',
+						'<',
+						'/dev/null'])
+
+	# other passes reduce the size
+	while(os.path.getsize('movie.gif') > 2097152):
+		subprocess.call(['convert',
+						'movie.gif',
+						'-resize',
+						'90%',
+						'-coalesce',
+						'-layers',
+						'optimize',
+						'movie.gif',
+						'>',
+						'/dev/null',
+						'2>&1',
+						'<',
+						'/dev/null'])
+	
+	#If first upload fails try again.
+	for i in range(0,2):
+		while True:	
+			try:
+				response = requests.post(
+					url,
+					headers = headers,
+					data = {
+						'key': API_KEY,
+						'image': b64encode(open('movie.gif', 'rb').read()),
+						'type': 'base64',
+						'name': 'movie.gif',
+						'title': title
+					}
+				)
+			except requests.exceptions.ConnectionError:
+				# try again.
+				continue
+			break;
+
+
+	try:
+		res_json = response.json()
+		link = res_json['data']['link']
+	except ValueError:
+		# try again.
+		continue
+
+	print link
+
+	twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+
+	status = title +" " + link + " " + str(hour) +":"+str(mins)+":"+str(secs)+ ' in. #moviegif #gif'
+
+	print "tweeting..."
+	twitter.update_status(status=status)
+
+	print "sleeping..."
+	# sleep 1 hour
+	time.sleep(3600)
